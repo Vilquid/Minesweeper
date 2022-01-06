5afdf803f21e81a39fa887f161b7081941dda712
Mathis <mathis.de-gueydon@isen.yncrea.fr>
2022-01-06 16:17:58 +0100

    Lombok
    Ajout de @Getter/@Setter pour une simplification visuelle.

--- src/main/java/fr/yncrea/cin3/minesweeper/domain/CelluleDomain.java
+++ src/main/java/fr/yncrea/cin3/minesweeper/domain/CelluleDomain.java
@@ -1,47 +1,15 @@
 package fr.yncrea.cin3.minesweeper.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class CelluleDomain
 {
 	private boolean revelee;
 	private int minesAutour;
-
-	public boolean estRevelee() {
-		return revelee;
-	}
-
-	public void setRevele(boolean revelee) {
-		this.revelee = revelee;
-	}
-
-	public int getMinesAutour() {
-		return minesAutour;
-	}
-
-	public void setMinesAutour(int minesAutour) {
-		this.minesAutour = minesAutour;
-	}
-
-	public boolean isMine() {
-		return mine;
-	}
-
-	public void setMine(boolean mine) {
-		this.mine = mine;
-	}
-
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
-
 	private boolean mine;
 	private boolean flag;
-
-	public boolean Cellule(boolean mine)
-	{
-		return this.mine = mine;
-	}
 }
